b4447ddf23808f9f7344517c50c8ee793e1a0e0f
Niklas Appelmann <niklasappelmann@gmail.com>
2020-04-28 19:33:25 +0200

    Fix router

--- app/client/main.tsx
+++ app/client/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
 import "react-datepicker/dist/react-datepicker.css"
+import './router'
 
 import App from '/imports/ui/App'
 
