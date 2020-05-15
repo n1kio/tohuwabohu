400337f2e2000bcab4bf9b4c069528ef106962ae
Niklas Appelmann <niklasappelmann@gmail.com>
2020-05-15 15:29:12 +0200

    Add explanation in steps to startpage

--- app/imports/ui/Dashboard.tsx
+++ app/imports/ui/Dashboard.tsx
@@ -7,7 +7,26 @@ import { ButtonPrimary, Spacer } from '/imports/ui/Primitives'
 const Dashboard = () => (
     <Layout>
         <Spacer/>
-        <h1>Einfach Online Freizeit organisieren!</h1>
+        <h1>Einfach Online-Freizeit organisieren!</h1>
+        <h2>In 3 einfachen Schritten</h2>
+        <ol>
+            <li>Event erstellen</li>
+            <li>Link an Freunde schicken</li>
+            <li>Einen passenden Zeitpunkt terminieren</li>
+        </ol>
+        <div className="spaced">
+            <ButtonPrimary onClick={() => {
+                FlowRouter.go('new-event')
+            }}>Event erstellen</ButtonPrimary>
+        </div>
+        <h2>Gibt es so etwas nicht schon?</h2>
+        <p>
+            Tohuwabohu hebt sich von anderen Tools dadurch ab, dass die Terminfindung nicht erfordert weit in die Zukunft vorrausplanen zu müssen.
+        </p>
+        <p>
+            Als Organisator machst du ein paar Vorschläge und sollten die nicht passen, dann können die Teilnehmer andere Vorschläge amchen. Wenn ein Zeitpunkt gefunden wurde kannst du den Zeitpunkt terminieren und alle Teilnehmer erhalten eine E-Mail als Bestätigung.
+        </p>
+        <h2>Hintergrund</h2>
         <p>
             Ein Treffen mit vielen Personen zu organisieren kann anstrengend sein.
         </p>
